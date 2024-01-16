63e400fbd443aa7b6c2b31f302e138b90c7f9841
Bassam-Z <bassamz.ali@hotmail.com>
2024-01-16 20:12:37 +0100

    add filter

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,22 @@
             <h1>Pinia Tasks</h1>
         </header>
 
+        <!-- filter -->
+        <nav class="filter">
+            <button @click="filter = 'all'">All tasks</button>
+            <button @click="filter = 'favs'">Fav tasks</button>
+        </nav>
+
         <!-- task list -->
-        <div class="task-list">
-            <div v-for="task in taskStore.tasks" :key="task.id">
+        <div v-if="filter === 'all'" class="task-list">
+            <p>Your have {{ taskStore.totalCount }} tasks left to do</p>
+            <div v-for="task in taskStore.favs" :key="task.id">
+                <TaskDetails :task="task"/>
+            </div>
+        </div>
+        <div v-if="filter === 'favs'" class="task-list">
+            <p>Your have {{ taskStore.favCount }} tasks left to do</p>
+            <div v-for="task in taskStore.favs" :key="task.id">
                 <TaskDetails :task="task"/>
             </div>
         </div>
@@ -18,12 +31,14 @@
 <script>
 import TaskDetails from '@/components/TaskDetails.vue'
 import { useTaskStore } from './stores/TasksStore'
+import { ref } from 'vue'
     export default {
         components: { TaskDetails },
         setup () {
             const taskStore = useTaskStore()
+            const filter = ref('all')
 
-            return { taskStore }
+            return { taskStore, filter }
         }
     }
 </script>
