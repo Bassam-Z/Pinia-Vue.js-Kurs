42a6f9790ae9581d67b29215ffc989c489950ba2
Bassam-Z <bassamz.ali@hotmail.com>
2024-01-27 01:30:55 +0100

    async Actions - Restting

--- src/App.vue
+++ src/App.vue
@@ -17,18 +17,26 @@
             <button @click="filter = 'favs'">Fav tasks</button>
         </nav>
 
+        <!-- Loading -->
+        <div v-if="isLoading" class="loading">
+            Loading Tasks...
+        </div>
+
         <!-- task list -->
         <div v-if="filter === 'all'" class="task-list">
-            <p>You have {{ taskStore.totalCount }} tasks left to do</p>
-            <div v-for="task in taskStore.tasks" :key="task.id">
+            <p>You have {{ totalCount }} tasks left to do</p>
+            <div v-for="task in tasks" :key="task.id">
                 <TaskDetails :task="task"/>
             </div>
         </div>
         <div v-if="filter === 'favs'" class="task-list">
-            <p>You have {{ taskStore.favCount }} tasks left to do</p>
-            <div v-for="task in taskStore.favs" :key="task.id">
+            <p>You have {{ favCount }} tasks left to do</p>
+            <div v-for="task in favs" :key="task.id">
                 <TaskDetails :task="task"/>
             </div>
+        </div>
+        <div class="filter">
+            <button  @click="taskStore.$reset">reset State</button>
         </div>
     </main>
 </template>
@@ -38,13 +46,20 @@ import TaskDetails from '@/components/TaskDetails.vue'
 import { useTaskStore } from './stores/TasksStore'
 import TaskForm from './components/TaskForm.vue'
 import { ref } from 'vue'
+import { storeToRefs } from 'pinia'
     export default {
         components: { TaskDetails, TaskForm },
         setup () {
             const taskStore = useTaskStore()
+
+            const { tasks, isLoading, favs, totalCount, favCount } = storeToRefs(taskStore)
+
+            // feth tasks
+            taskStore.getTasks()
+
             const filter = ref('all')
 
-            return { taskStore, filter }
+            return { taskStore, filter,tasks, isLoading, favs, totalCount, favCount }
         }
     }
 </script>
